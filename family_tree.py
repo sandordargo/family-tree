96e2c5b445ecbae7ed3e852c4b2c688ac6b76b23
Sandor Dargo <sandor.dargo@gmail.com>
2016-05-06 09:16:30 +0200

    refactor: Subclass vertical sort from FamilyTree class

--- family_tree.py
+++ family_tree.py
@@ -1,6 +1,6 @@
 from database import database_layer
-import random
 import horizontal_sorter
+import vertical_sorter
 
 
 class FamilyTree(object):
@@ -9,7 +9,8 @@ class FamilyTree(object):
         self.db = database_layer.DatabaseConnection()
         self.nodes = self.read_persons()
         self.edges = self.read_relationships()
-        self.levels_on_tree = self.build_levels_dictionary()
+        my_vertical_sorter = vertical_sorter.VerticalSorter(self.edges)
+        self.levels_on_tree = my_vertical_sorter.get_levels_dictionary()
         my_horizontal_sorter = horizontal_sorter.HorizontalSorter(self.levels_on_tree, self.edges)
         self.person_horizontal_position_dict = my_horizontal_sorter.get_person_horizontal_position_dict()
         self.size = 2
@@ -47,42 +48,42 @@ class FamilyTree(object):
             edge['size'] = self.size
         return family_tree_json
 
-    def build_levels_dictionary(self):
-        person_level_dict = dict()
-        visited_relationships_list = list()
-        reachable_persons_list = list()
-
-        # get first relationship, put high enough for a worst case scenario, where everyone would go under it
-        # target to up or same level based on type of relationship
-        first_relationship = self.edges[0]
-        person_level_dict[first_relationship['source']] = len(self.edges) + 1
-        if first_relationship['type'] == 'CHILD_OF':
-            person_level_dict[first_relationship['target']] = person_level_dict[first_relationship['source']] - 1
-        if first_relationship['type'] == 'MARRIED':
-            person_level_dict[first_relationship['target']] = person_level_dict[first_relationship['source']]
-        visited_relationships_list.append(first_relationship['id'])
-        reachable_persons_list.append(first_relationship['source'])
-        reachable_persons_list.append(first_relationship['target'])
-
-        # look for adjacent relationship until there is any non-visited
-        # TODO if not and there is, get any relationship
-        # TODO levels should start from zero
-        while len(visited_relationships_list) < len(self.edges):
-            for relationship in self.edges:
-                if relationship['id'] not in visited_relationships_list and \
-                    (relationship['source'] in reachable_persons_list or
-                        relationship['target'] in reachable_persons_list):
-                    visited_relationships_list.append(relationship['id'])
-                    if relationship['type'] == 'CHILD_OF' and relationship['target'] in reachable_persons_list:
-                        person_level_dict[relationship['source']] = person_level_dict[relationship['target']] + 1
-                        reachable_persons_list.append(relationship['source'])
-                    elif relationship['type'] == 'CHILD_OF' and relationship['source'] in reachable_persons_list:
-                        person_level_dict[relationship['target']] = person_level_dict[relationship['source']] - 1
-                        reachable_persons_list.append(relationship['target'])
-                    elif relationship['type'] == 'MARRIED' and relationship['target'] in reachable_persons_list:
-                        person_level_dict[relationship['source']] = person_level_dict[relationship['target']]
-                        reachable_persons_list.append(relationship['source'])
-                    elif relationship['type'] == 'MARRIED' and relationship['source'] in reachable_persons_list:
-                        person_level_dict[relationship['target']] = person_level_dict[relationship['source']]
-                        reachable_persons_list.append(relationship['target'])
-        return person_level_dict
+    # def build_levels_dictionary(self):
+    #     person_level_dict = dict()
+    #     visited_relationships_list = list()
+    #     reachable_persons_list = list()
+    #
+    #     # get first relationship, put high enough for a worst case scenario, where everyone would go under it
+    #     # target to up or same level based on type of relationship
+    #     first_relationship = self.edges[0]
+    #     person_level_dict[first_relationship['source']] = len(self.edges) + 1
+    #     if first_relationship['type'] == 'CHILD_OF':
+    #         person_level_dict[first_relationship['target']] = person_level_dict[first_relationship['source']] - 1
+    #     if first_relationship['type'] == 'MARRIED':
+    #         person_level_dict[first_relationship['target']] = person_level_dict[first_relationship['source']]
+    #     visited_relationships_list.append(first_relationship['id'])
+    #     reachable_persons_list.append(first_relationship['source'])
+    #     reachable_persons_list.append(first_relationship['target'])
+    #
+    #     # look for adjacent relationship until there is any non-visited
+    #     # TODO if not and there is, get any relationship
+    #     # TODO levels should start from zero
+    #     while len(visited_relationships_list) < len(self.edges):
+    #         for relationship in self.edges:
+    #             if relationship['id'] not in visited_relationships_list and \
+    #                 (relationship['source'] in reachable_persons_list or
+    #                     relationship['target'] in reachable_persons_list):
+    #                 visited_relationships_list.append(relationship['id'])
+    #                 if relationship['type'] == 'CHILD_OF' and relationship['target'] in reachable_persons_list:
+    #                     person_level_dict[relationship['source']] = person_level_dict[relationship['target']] + 1
+    #                     reachable_persons_list.append(relationship['source'])
+    #                 elif relationship['type'] == 'CHILD_OF' and relationship['source'] in reachable_persons_list:
+    #                     person_level_dict[relationship['target']] = person_level_dict[relationship['source']] - 1
+    #                     reachable_persons_list.append(relationship['target'])
+    #                 elif relationship['type'] == 'MARRIED' and relationship['target'] in reachable_persons_list:
+    #                     person_level_dict[relationship['source']] = person_level_dict[relationship['target']]
+    #                     reachable_persons_list.append(relationship['source'])
+    #                 elif relationship['type'] == 'MARRIED' and relationship['source'] in reachable_persons_list:
+    #                     person_level_dict[relationship['target']] = person_level_dict[relationship['source']]
+    #                     reachable_persons_list.append(relationship['target'])
+    #     return person_level_dict
---
+++ vertical_sorter.py
@@ -0,0 +1,48 @@
+class VerticalSorter(object):
+
+    def __init__(self, edges):
+        self.edges = edges
+        self.levels_dictionary = self.build_levels_dictionary()
+
+    def get_levels_dictionary(self):
+        return self.levels_dictionary
+
+    def build_levels_dictionary(self):
+        person_level_dict = dict()
+        visited_relationships_list = list()
+        reachable_persons_list = list()
+
+        # get first relationship, put high enough for a worst case scenario, where everyone would go under it
+        # target to up or same level based on type of relationship
+        first_relationship = self.edges[0]
+        person_level_dict[first_relationship['source']] = len(self.edges) + 1
+        if first_relationship['type'] == 'CHILD_OF':
+            person_level_dict[first_relationship['target']] = person_level_dict[first_relationship['source']] - 1
+        if first_relationship['type'] == 'MARRIED':
+            person_level_dict[first_relationship['target']] = person_level_dict[first_relationship['source']]
+        visited_relationships_list.append(first_relationship['id'])
+        reachable_persons_list.append(first_relationship['source'])
+        reachable_persons_list.append(first_relationship['target'])
+
+        # look for adjacent relationship until there is any non-visited
+        # TODO if not and there is, get any relationship
+        # TODO levels should start from zero
+        while len(visited_relationships_list) < len(self.edges):
+            for relationship in self.edges:
+                if relationship['id'] not in visited_relationships_list and \
+                        (relationship['source'] in reachable_persons_list or
+                                 relationship['target'] in reachable_persons_list):
+                    visited_relationships_list.append(relationship['id'])
+                    if relationship['type'] == 'CHILD_OF' and relationship['target'] in reachable_persons_list:
+                        person_level_dict[relationship['source']] = person_level_dict[relationship['target']] + 1
+                        reachable_persons_list.append(relationship['source'])
+                    elif relationship['type'] == 'CHILD_OF' and relationship['source'] in reachable_persons_list:
+                        person_level_dict[relationship['target']] = person_level_dict[relationship['source']] - 1
+                        reachable_persons_list.append(relationship['target'])
+                    elif relationship['type'] == 'MARRIED' and relationship['target'] in reachable_persons_list:
+                        person_level_dict[relationship['source']] = person_level_dict[relationship['target']]
+                        reachable_persons_list.append(relationship['source'])
+                    elif relationship['type'] == 'MARRIED' and relationship['source'] in reachable_persons_list:
+                        person_level_dict[relationship['target']] = person_level_dict[relationship['source']]
+                        reachable_persons_list.append(relationship['target'])
+        return person_level_dict
