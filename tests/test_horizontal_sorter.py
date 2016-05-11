d95d6bd534717d41f44f0905bb63bfffc025498a
Sandor Dargo <sandor.dargo@gmail.com>
2016-05-11 09:00:28 +0200

    doc and refactor and test: horizontal sorter

--- tests/test_horizontal_sorter.py
+++ tests/test_horizontal_sorter.py
@@ -6,26 +6,33 @@ class TestHorizontalSorter(unittest.TestCase):
     def setUp(self):
         pass
 
+    @staticmethod
+    def only_even_numbers_in_dict(dict_to_check):
+        for key in dict_to_check:
+            if dict_to_check[key] % 2 != 0:
+                return False
+        return True
+
     def test_are_there_persons_at_the_same_positions_false(self):
         position_person_dict = {1: ['5', '3'], 2: ['1', '2'], 3: ['4']}
-        levels_on_tree = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 4}
+        person_level_dict = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 4}
         edges = {}
 
-        sorter = horizontal_sorter.HorizontalSorter(levels_on_tree, edges)
+        sorter = horizontal_sorter.HorizontalSorter(person_level_dict, edges)
         self.assertFalse(sorter.are_there_persons_at_the_same_positions(position_person_dict))
 
     def test_are_there_persons_at_the_same_positions_true(self):
         position_person_dict = {1: ['5', '3'], 2: ['1', '2'], 3: ['4']}
-        levels_on_tree = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 3}
+        person_level_dict = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 3}
         edges = {}
 
-        sorter = horizontal_sorter.HorizontalSorter(levels_on_tree, edges)
+        sorter = horizontal_sorter.HorizontalSorter(person_level_dict, edges)
         self.assertTrue(sorter.are_there_persons_at_the_same_positions(position_person_dict))
 
     def test_get_level_of_person(self):
-        levels_on_tree = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 4}
+        person_level_dict = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 4}
         edges = {}
-        sorter = horizontal_sorter.HorizontalSorter(levels_on_tree, edges)
+        sorter = horizontal_sorter.HorizontalSorter(person_level_dict, edges)
         expected_level = 4
         retrieved_level = sorter.get_level_of_person('5')
 
@@ -33,24 +40,129 @@ class TestHorizontalSorter(unittest.TestCase):
 
     def test_get_position_with_multiple_persons_raise(self):
         position_person_dict = {1: ['5', '3'], 2: ['1', '2'], 3: ['4']}
-        levels_on_tree = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 4}
+        person_level_dict = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 4}
         edges = {}
 
-        sorter = horizontal_sorter.HorizontalSorter(levels_on_tree, edges)
+        sorter = horizontal_sorter.HorizontalSorter(person_level_dict, edges)
         retrieved_persons = sorter.get_position_with_multiple_persons(position_person_dict)
 
         self.assertIsNone(retrieved_persons)
 
     def test_get_position_with_multiple_persons_true(self):
         position_person_dict = {1: ['5', '3'], 2: ['1', '2'], 3: ['4']}
-        levels_on_tree = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 3}
+        person_level_dict = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 3}
         edges = {}
-        sorter = horizontal_sorter.HorizontalSorter(levels_on_tree, edges)
+        sorter = horizontal_sorter.HorizontalSorter(person_level_dict, edges)
         expected_persons = ['5', '3']
 
         retrieved_persons = sorter.get_position_with_multiple_persons(position_person_dict)
 
         self.assertEqual(retrieved_persons, expected_persons)
+
+    def test_build_person_dict(self):
+        person_level_dict = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 3}
+        edges = {}
+
+        sorter = horizontal_sorter.HorizontalSorter(person_level_dict, edges)
+
+        expected_output = {1: ['1'], 2: ['2'], 3: ['3', '5'], 4: ['4']}
+
+        retrieved_output = sorter.build_level_person_dict()
+
+        self.assertEqual(retrieved_output, expected_output)
+
+    def test_assign_random_x_positions(self):
+        person_level_dict = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 3}
+        edges = {}
+        expected_number_of_persons = len(person_level_dict)
+
+        sorter = horizontal_sorter.HorizontalSorter(person_level_dict, edges)
+        person_horizontal_position_dict = sorter.assign_random_x_positions()
+
+        retrieved_number_of_persons = len(person_horizontal_position_dict)
+        only_even_values_in_dict = self.only_even_numbers_in_dict(person_horizontal_position_dict)
+
+        self.assertEqual(retrieved_number_of_persons, expected_number_of_persons)
+        self.assertTrue(only_even_values_in_dict)
+
+    def test_build_position_person_dict(self):
+        person_level_dict = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 3}
+        edges = {}
+        sorter = horizontal_sorter.HorizontalSorter(person_level_dict, edges)
+        sorter.person_horizontal_position_dict = {'1': 0, '3': 0, '2': 2, '5': 4, '4': 0}
+
+        expected_position_person_dict = {0: ['1', '3', '4'], 2: ['2'], 4: ['5']}
+
+        retrieved_position_person_dict = sorter.build_position_person_dict()
+
+        self.assertEqual(retrieved_position_person_dict, expected_position_person_dict)
+
+#
+# def move_persons_from_same_level_and_position(self):
+#     while self.are_there_persons_at_the_same_positions(self.position_person_dict):
+#         position_with_multiple_persons = self.get_position_with_multiple_persons(self.position_person_dict)
+#         i = 0
+#         for person in position_with_multiple_persons:
+#             print('persons with same position: {}'.format(position_with_multiple_persons))
+#             self.move_person_on_horizontal_axis(person, -1 * i * 2)
+#             i += 1
+
+    def test_move_persons_from_same_level_and_position(self):
+        pass
+
+    def test_move_person_on_horizontal_axis_single_move(self):
+        person_level_dict = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 3}
+        edges = {}
+        sorter = horizontal_sorter.HorizontalSorter(person_level_dict, edges)
+        sorter.person_horizontal_position_dict = {'1': 0, '3': 0, '2': 2, '5': 4, '4': 0}
+        sorter.position_person_dict = {0: ['1', '3', '4'], 2: ['2'], 4: ['5']}
+
+        expected_position_person_dict = {0: ['1', '3'], 2: ['2', '4'], 4: ['5']}
+        expected_person_horizontal_position_dict = {'1': 0, '3': 0, '2': 2, '5': 4, '4': 2}
+
+        sorter.move_person_on_horizontal_axis('4', 2)
+
+        retrieved_position_person_dict = sorter.position_person_dict
+        retrieved_person_horizontal_position_dict = sorter.person_horizontal_position_dict
+
+        self.assertEqual(retrieved_position_person_dict, expected_position_person_dict)
+        self.assertEqual(retrieved_person_horizontal_position_dict, expected_person_horizontal_position_dict)
+
+    def test_move_person_on_horizontal_axis_double_move(self):
+        person_level_dict = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 3}
+        edges = {}
+        sorter = horizontal_sorter.HorizontalSorter(person_level_dict, edges)
+        sorter.person_horizontal_position_dict = {'1': 0, '3': 2, '2': 2, '5': 4, '4': 0}
+        sorter.position_person_dict = {0: ['1', '4'], 2: ['2', '3'], 4: ['5']}
+
+        expected_position_person_dict = {0: ['1', '4'], 2: ['2'], 4: ['3'], 6: ['5']}
+        expected_person_horizontal_position_dict = {'1': 0, '3': 4, '2': 2, '5': 6, '4': 0}
+
+        sorter.move_person_on_horizontal_axis('3', 2)
+
+        retrieved_position_person_dict = sorter.position_person_dict
+        retrieved_person_horizontal_position_dict = sorter.person_horizontal_position_dict
+
+        self.assertEqual(retrieved_position_person_dict, expected_position_person_dict)
+        self.assertEqual(retrieved_person_horizontal_position_dict, expected_person_horizontal_position_dict)
+
+    def test_move_person_on_horizontal_axis_to_position(self):
+        person_level_dict = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 3}
+        edges = {}
+        sorter = horizontal_sorter.HorizontalSorter(person_level_dict, edges)
+        sorter.person_horizontal_position_dict = {'1': 0, '3': 2, '2': 2, '5': 4, '4': 0}
+        sorter.position_person_dict = {0: ['1', '4'], 2: ['2', '3'], 4: ['5']}
+
+        expected_position_person_dict = {0: ['1', '4'], 2: ['2'], 4: ['3'], 6: ['5']}
+        expected_person_horizontal_position_dict = {'1': 0, '3': 4, '2': 2, '5': 6, '4': 0}
+
+        sorter.move_person_on_horizontal_axis_to_position('3', 4)
+
+        retrieved_position_person_dict = sorter.position_person_dict
+        retrieved_person_horizontal_position_dict = sorter.person_horizontal_position_dict
+
+        self.assertEqual(retrieved_position_person_dict, expected_position_person_dict)
+        self.assertEqual(retrieved_person_horizontal_position_dict, expected_person_horizontal_position_dict)
 
 
 if __name__ == '__main__':
--- tree/horizontal_sorter.py
+++ tree/horizontal_sorter.py
@@ -6,7 +6,7 @@ class HorizontalSorter(object):
 
     def __init__(self, levels_on_tree, edges):
         self.db = database_layer.DatabaseConnection()
-        self.levels_on_tree = levels_on_tree
+        self.person_level_tree = levels_on_tree
         self.edges = edges
         self.person_horizontal_position_dict = dict()
         self.position_person_dict = dict()
@@ -19,38 +19,11 @@ class HorizontalSorter(object):
         # TODO add tests
         # TODO every children should be under his parents or spouse's parents
         # TODO siblings next to each other (even)
-        # TODO maintain position_person_dict
-        self.assign_random_x_positions()
-        self.build_position_person_dict()
+        # TODO make sure that between siblings there are nobody else only spouses
+        self.person_horizontal_position_dict = self.assign_random_x_positions()
+        self.position_person_dict = self.build_position_person_dict()
         self.put_children_under_parents()
-        self.move_persons_from_same_level_and_position()
         self.move_married_people_next_to_each_other()
- #       self.put_children_under_parents()
-
-    def move_person_on_horizontal_axis_to_position(self, person_id, new_position):
-        old_position = self.person_horizontal_position_dict[person_id]
-        change = new_position - old_position
-        self.move_person_on_horizontal_axis(person_id, change)
-
-    def move_person_on_horizontal_axis(self, person_id, change):
-        if str(self.person_horizontal_position_dict[person_id] + change) in self.position_person_dict:
-            for person in self.position_person_dict[self.person_horizontal_position_dict[person_id]]:
-                self.move_person_on_horizontal_axis(person, change)
-            old_position = self.person_horizontal_position_dict[person_id]
-            new_position = self.person_horizontal_position_dict[person_id] + change
-            self.person_horizontal_position_dict[person_id] = new_position
-            self.position_person_dict[old_position].remove(person_id)
-            if new_position not in self.position_person_dict:
-                self.position_person_dict[new_position] = list()
-            self.position_person_dict[new_position].append(person_id)
-        else:
-            old_position = self.person_horizontal_position_dict[person_id]
-            new_position = self.person_horizontal_position_dict[person_id] + change
-            self.person_horizontal_position_dict[person_id] = new_position
-            self.position_person_dict[old_position].remove(person_id)
-            if new_position not in self.position_person_dict:
-                self.position_person_dict[new_position] = list()
-            self.position_person_dict[new_position].append(person_id)
 
     def move_married_people_next_to_each_other(self):
         for inner_level in self.level_person_dict:
@@ -61,26 +34,6 @@ class HorizontalSorter(object):
                     new_position = self.person_horizontal_position_dict[relationship['source']] + 1
                     self.move_person_on_horizontal_axis_to_position(relationship['target'], new_position)
 
-    # TODO make it recursive, if there is someone there, shift her
-    # TODO move based on level
-    def move_persons_from_same_level_and_position(self):
-        while self.are_there_persons_at_the_same_positions(self.position_person_dict):
-            position_with_multiple_persons = self.get_position_with_multiple_persons(self.position_person_dict)
-            i = 0
-            for person in position_with_multiple_persons:
-                print('persons with same position: {}'.format(position_with_multiple_persons))
-                self.move_person_on_horizontal_axis(person, -1 * i * 2)
-                i += 1
-
-    def build_position_person_dict(self):
-        for level in self.level_person_dict:
-            for person in self.level_person_dict[level]:
-                if self.person_horizontal_position_dict[person] in self.position_person_dict:
-                    self.position_person_dict[self.person_horizontal_position_dict[person]].append(person)
-                else:
-                    self.position_person_dict[self.person_horizontal_position_dict[person]] = list()
-                    self.position_person_dict[self.person_horizontal_position_dict[person]].append(person)
-
     def put_children_under_parents(self):
         for level in self.level_person_dict:
             for person in self.level_person_dict[level]:
@@ -90,22 +43,74 @@ class HorizontalSorter(object):
                     new_position = 0
                     for parent in parents:
                         new_position = self.person_horizontal_position_dict[parent]
-                    #self.person_horizontal_position_dict[person] = new_position
                     self.move_person_on_horizontal_axis_to_position(person, new_position)
 
-    def assign_random_x_positions(self):
+    def move_person_on_horizontal_axis_to_position(self, person_id, new_position):
+        """
+        Moves a person to the given position.
+        If there is already someone at that position, he will be moved with the same number of positions.
+        """
+        old_position = self.person_horizontal_position_dict[person_id]
+        change = new_position - old_position
+        self.move_person_on_horizontal_axis(person_id, change)
+
+    def move_person_on_horizontal_axis(self, person_id, change):
+        """
+        Moves a person by the given change in position.
+        If there is already someone at that position, he will be moved with the same number of positions.
+        """
+        if change == 0:
+            return
+        if self.person_horizontal_position_dict[person_id] + change in self.position_person_dict and \
+                        len(self.position_person_dict[self.person_horizontal_position_dict[person_id] + change]) > 0:
+            for person in self.position_person_dict[self.person_horizontal_position_dict[person_id] + change]:
+                if self.get_level_of_person(person_id) == self.get_level_of_person(person):
+                    self.move_person_on_horizontal_axis(person, change)
+
+        old_position = self.person_horizontal_position_dict[person_id]
+        new_position = self.person_horizontal_position_dict[person_id] + change
+        self.person_horizontal_position_dict[person_id] = new_position
+        self.position_person_dict[old_position].remove(person_id)
+        if new_position not in self.position_person_dict:
+            self.position_person_dict[new_position] = list()
+        self.position_person_dict[new_position].append(person_id)
+
+    def build_position_person_dict(self):
+        """
+        Returns a dictionary of horizontal positions (key) and lists of perons (value) in the given position
+        """
+        position_person_dict = dict()
         for level in self.level_person_dict:
             for person in self.level_person_dict[level]:
-                # TODO make sure uniqueness
-                random_coefficient = random.randrange(0, 2 * len(self.level_person_dict[level]) + 1, 2)
-                self.person_horizontal_position_dict[person] = random_coefficient
+                if self.person_horizontal_position_dict[person] not in position_person_dict:
+                    position_person_dict[self.person_horizontal_position_dict[person]] = list()
+                position_person_dict[self.person_horizontal_position_dict[person]].append(person)
+        return position_person_dict
+
+    def assign_random_x_positions(self):
+        """
+        Returns a dictionary of persons (key) and randomly assigned horizontal positions (values)
+        Although the values are randomly assigned, they range from 0 to twice the number of the persons on a given level
+         and each value most be even and cannot appear more than once
+        """
+        person_horizontal_position_dict = dict()
+        for level in self.level_person_dict:
+            number_of_persons = len(self.level_person_dict[level])
+            random_positions = random.sample(range(0, 2 * number_of_persons + 1, 2), number_of_persons)
+            for person in self.level_person_dict[level]:
+                person_horizontal_position_dict[person] = random_positions.pop()
+        return person_horizontal_position_dict
 
     def build_level_person_dict(self):
+        """
+        Based on levels_on_tree dictionary (key: person, value: its level on tree) builds another dict
+         where the keys are the levels and values are the person sitting on a given level
+        """
         level_person_dict = dict()
-        for person in self.levels_on_tree:
-            if self.levels_on_tree[person] not in level_person_dict:
-                level_person_dict[self.levels_on_tree[person]] = list()
-            level_person_dict[self.levels_on_tree[person]].append(person)
+        for person in self.person_level_tree:
+            if self.person_level_tree[person] not in level_person_dict:
+                level_person_dict[self.person_level_tree[person]] = list()
+            level_person_dict[self.person_level_tree[person]].append(person)
         return level_person_dict
 
     def are_there_persons_at_the_same_positions(self, position_persons_dict):
@@ -119,7 +124,7 @@ class HorizontalSorter(object):
         """
         Returns the level of the given person
         """
-        return self.levels_on_tree[person]
+        return self.person_level_tree[person]
 
     def get_position_with_multiple_persons(self, position_persons_dict):
         """
